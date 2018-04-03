4e3c5f365ad9a2e9957ab4150224c4baa6c2c7af
Thomas Hoen <tom@givecorps.com>
2018-04-02 22:12:36 -0400

    Format the response as a deep Openstruct

--- lib/zuora/error_handler.rb
+++ lib/zuora/error_handler.rb
@@ -4,7 +4,7 @@ module Zuora
 
   class ErrorHandler
     def self.handle_response(response)
-      return response.except("success") if response["success"]
+      return formatted_response(response.except("success")) if response["success"]
 
       if response["success"].nil?
         raise Zuora::UnknownError.new(response)
@@ -14,6 +14,11 @@ module Zuora
       end
     end
 
+    def self.formatted_response(response)
+      snake_cased_response = response.deep_transform_keys!(&:underscore)
+      JSON.parse(snake_cased_response.to_json, object_class: OpenStruct)
+    end
+
   private
 
     def self.humanize_reason(response)
@@ -21,5 +26,6 @@ module Zuora
         "Error #{reason_hash['code']}: #{reason_hash['message'].humanize}"
       end.join("\n")
     end
+
   end
 end
